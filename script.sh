2db33071635778e4d0a2e3bcba86a9043a79afeb
Bruna Siqueira <brunasiqueira3103@gmail.com>
2022-12-13 14:16:47 -0300

    alteração de dados

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "\nCriando as imagens...\n"
+
+docker image build -t cleiconde/projeto-backend:1.0 backend/.
+docker image build -t cleiconde/projeto-database:1.0 database/.
+
+echo -e "\nRealizando o push das imagens...\n"
+
+docker push brunasiqueira3103/dio-k8s-backend:1.0
+docker push brunasiqueira3103/dio-k8s-database:1.0
+
+echo -e "\nCriando serviços no cluster Kubernetes...\n"
+
+kubectl apply -f ./services.yml
+
+echo -e "\nRealizando o deploy...\n"
+
+kubectl apply -f ./deployment.yml
